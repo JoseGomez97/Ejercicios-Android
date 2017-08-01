3a28b8d663fd3533ece5997e12ee770c743d5ac0
Jose <josegomezgadea@gmail.com>
2017-08-01 14:16:39 +0200

    Guardado desde Android Studio

--- Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
+++ Lesson02-GitHub-Repo-Search/T02.01-Exercise-CreateLayout/app/src/main/java/com/example/android/datafrominternet/MainActivity.java
@@ -17,13 +17,17 @@ package com.example.android.datafrominternet;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (26) Create an EditText variable called mSearchBoxEditText
-
-    // TODO (27) Create a TextView variable called mUrlDisplayTextView
-    // TODO (28) Create a TextView variable called mSearchResultsTextView
+    // (26) Create an EditText variable called mSearchBoxEditText
+    EditText mSearchBoxEditText;
+    // (27) Create a TextView variable called mUrlDisplayTextView
+    TextView mUrlDisplayTextView;
+    // (28) Create a TextView variable called mSearchResultsTextView
+    TextView mSearchResultsTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
